c40f7e0b25795af3d88d526181ba6b7705cbccd8
Liezl Lizardo <llizardo@byupathway.edu>
2026-08-07 17:36:09 +0800

    Create ASP.NET Core MVC solution

--- src/IBMORS.Web/Program.cs
+++ src/IBMORS.Web/Program.cs
@@ -1,48 +1,29 @@
-using Microsoft.AspNetCore.Authentication.Cookies;
-
 var builder = WebApplication.CreateBuilder(args);
 
-// Add MVC Services
+// Add services to the container.
 builder.Services.AddControllersWithViews();
-
-// Cookie Authentication
-builder.Services
-    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(options =>
-    {
-        options.LoginPath = "/Account/Login";
-        options.LogoutPath = "/Account/Logout";
-        options.AccessDeniedPath = "/Account/AccessDenied";
-
-        options.Cookie.Name = "IBMORS.Auth";
-        options.ExpireTimeSpan = TimeSpan.FromHours(8);
-        options.SlidingExpiration = true;
-    });
-
-// Authorization
-builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
-// Configure HTTP Request Pipeline
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
 app.UseRouting();
-
-app.UseAuthentication();
 
 app.UseAuthorization();
 
+app.MapStaticAssets();
+
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
 
 app.Run();
